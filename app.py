acae4e009878598675d70da9cbc616bcdfa6b390
FerPedron <105116340+FerPedron@users.noreply.github.com>
2022-05-21 13:11:43 -0300

    Dockerizando a app

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
 csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
-
-
-
+    return "LabDevOps - Turma 05"
 
 if __name__ == '__main__':
-    app.run()
+    port = os.getenv('PORT')
+    app.run('0.0.0.0', port=port)
